1aa92a4f3da690b42902d99a56b887f74d1d32f0
兮玥 <190785909@qq.com>
2024-01-13 16:32:38 +0800

    copyright更新

--- chestnut-ui/src/views/login.vue
+++ chestnut-ui/src/views/login.vue
@@ -59,7 +59,7 @@
     </el-form>
     <!--  底部  -->
     <div class="el-login-footer">
-      <span>Copyright © 2022-2023 ChestnutCMS (1000mz.com) All Rights Reserved.</span>
+      <span>Copyright © 2022-2024 ChestnutCMS (1000mz.com) All Rights Reserved.</span>
     </div>
   </div>
 </template>
