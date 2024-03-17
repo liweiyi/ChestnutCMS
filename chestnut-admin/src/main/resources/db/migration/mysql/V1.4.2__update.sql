071733ce26b7bc116853d77189e32d545985d654
liweiyi <190785909@qq.com>
2024-03-17 17:01:33 +0800

    1.4.2 update sql 修正

--- chestnut-admin/src/main/resources/db/migration/mysql/V1.4.2__update.sql
+++ chestnut-admin/src/main/resources/db/migration/mysql/V1.4.2__update.sql
@@ -1,17 +1,6 @@
-CREATE TABLE `search_word` (
-  `word_id` bigint NOT NULL COMMENT '主键ID',
-  `word` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '搜索词',
-  `search_total` bigint NOT NULL COMMENT '历史累计搜索次数',
-  `top_flag` bigint NOT NULL COMMENT '置顶标识',
-  `top_date` datetime DEFAULT NULL COMMENT '置顶结束时间',
-  `source` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '来源标识',
-  `create_by` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '创建人',
-  `create_time` datetime NOT NULL COMMENT '创建时间',
-  `update_by` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '最后修改人',
-  `update_time` datetime DEFAULT NULL COMMENT '最后修改时间',
-  `remark` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '备注',
-  PRIMARY KEY (`word_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+ALTER TABLE search_word ADD COLUMN top_flag bigint;
+ALTER TABLE search_word ADD COLUMN top_date datetime;
+ALTER TABLE search_word ADD COLUMN source varchar(255);
 
 CREATE TABLE `search_word_hour_stat` (
   `stat_id` bigint NOT NULL AUTO_INCREMENT,
