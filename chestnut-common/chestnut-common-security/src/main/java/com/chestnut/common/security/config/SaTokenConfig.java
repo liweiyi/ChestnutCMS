21d93d92fe8f59d3370f1ea5769ed61149278a66
liweiyi <190785909@qq.com>
2025-08-02 15:54:58 +0800

    fixed: Argon2 config not init

--- chestnut-common/chestnut-common-security/src/main/java/com/chestnut/common/security/config/SaTokenConfig.java
+++ chestnut-common/chestnut-common-security/src/main/java/com/chestnut/common/security/config/SaTokenConfig.java
@@ -28,7 +28,6 @@ import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@RequiredArgsConstructor
 @EnableConfigurationProperties(SecurityProperties.class)
 public class SaTokenConfig implements WebMvcConfigurer {
 
